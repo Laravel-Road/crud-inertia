ac17efeada10b4bf3764406c4ac01996bc392c04
Pedro Augusto <pams.pedro@gmail.com>
2021-01-26 11:18:59 -0300

    customize brand

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -2,10 +2,10 @@
     <div>
         <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
             <div>
-                <jet-application-logo class="block h-12 w-auto" />
+                <jet-application-logo class="block w-auto" />
             </div>
 
-            <div class="mt-8 text-2xl">
+            <div class="text-2xl">
                 Welcome to your Jetstream application!
             </div>
 
