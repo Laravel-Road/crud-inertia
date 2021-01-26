1f2973bcb465abf1c2beec2c0855d884fc94b637
Pedro Augusto <pams.pedro@gmail.com>
2021-01-26 13:57:12 -0300

    add pagination

--- resources/js/Pages/Contacts/ContactList.vue
+++ resources/js/Pages/Contacts/ContactList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="contacts.length > 0">
+    <div v-if="contacts.total > 0">
         <jet-section-border />
 
         <jet-action-section class="mt-10 sm:mt-0">
@@ -13,12 +13,13 @@
 
             <template #content>
                 <div class="space-y-6">
-                    <div class="flex items-center justify-between" v-for="contact in contacts" :key="contact.id">
+                    <div class="flex items-center justify-between" v-for="contact in contacts.data" :key="contact.id">
                         <div class="flex items-start justify-betweenr">
                             <div class="w-64">{{ contact.name }}</div>
                             <div class="w-64">{{ contact.email }}</div>
                         </div>
                     </div>
+                    <pagination :links="contacts.links" />
                 </div>
             </template>
         </jet-action-section>
@@ -28,9 +29,11 @@
 <script>
     import JetActionSection from '@/Jetstream/ActionSection'
     import JetSectionBorder from '@/Jetstream/SectionBorder'
+    import Pagination from '@/Shared/Pagination'
 
     export default {
         components: {
+            Pagination,
             JetActionSection,
             JetSectionBorder,
         },
---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="mt-6 -mb-1 flex flex-wrap">
+        <template v-for="(link, key) in links">
+            <div v-if="link.url === null"
+                 :key="key"
+                 class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md"
+                 :class="{ 'ml-auto': link.label === 'Next' }">
+                <span v-html="link.label"></span>
+            </div>
+            <inertia-link v-else
+                          :key="key"
+                          class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                          :class="{ 'bg-white': link.active, 'ml-auto': link.label === 'Next' }"
+                          :href="link.url">
+                <span v-html="link.label"></span>
+            </inertia-link>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        links: Array,
+    },
+}
+</script>
