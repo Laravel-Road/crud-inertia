b1b01885e092d1d230e23b6850ebe69a4a3aff01
Pedro Augusto <pams.pedro@gmail.com>
2021-01-26 11:49:04 -0300

    create contact index component

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ContactStoreRequest;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        //
+        return Inertia::render('Contacts/Index', ['contacts' => Contact::all()]);
     }
 
     public function store(ContactStoreRequest $request)
---
+++ resources/js/Pages/Contacts/Index.vue
@@ -0,0 +1,35 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Contacts
+            </h2>
+        </template>
+
+        <div>
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                <contact-new></contact-new>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    import JetSectionBorder from '@/Jetstream/SectionBorder'
+    import ContactNew from './ContactNew'
+    // import ContactList from './ContactList'
+
+    export default {
+        props: [
+            'contacts',
+        ],
+
+        components: {
+            AppLayout,
+            JetSectionBorder,
+            ContactNew,
+            // ContactList,
+        },
+    }
+</script>
